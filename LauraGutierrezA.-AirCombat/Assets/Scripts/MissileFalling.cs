99cda6986bbf38d8939c21750e7f93678621e4ae
laurguad5 <36015596+laurguad5@users.noreply.github.com>
2018-04-11 21:40:50 -0500

    Aircombat
    Actividad 4

---
+++ LauraGutierrezA.-AirCombat/Assets/Scripts/MissileFalling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileFalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Mover hacia adelante 
+		this.transform.Translate(0.0f, 0.0f, 0.2f);
+	}
+	void OnCollisionEnter(Collision collision)  { 
+		
+		//Eliiminamos el piso
+		Destroy(collision.collider.gameObject);
+
+
+}
+}
